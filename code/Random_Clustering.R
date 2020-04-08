cd7f6e119955c1ca4860b5e8b8371160a1481710
Ian Buller, PhD, MA <ian.buller@nih.gov>
2020-04-08 13:02:28 -0400

    Moved custom function to own directory
    Initial commit of rand_cascon_neymscot() function

--- code/Random_Clustering.R
+++ code/Random_Clustering.R
@@ -8,12 +8,13 @@
 # Recently modified on: April 8, 2020
 #
 # Notes:
-# A) 4/7/20 (IB) - Potential simulation scheme for sparrpoweR package 0.0.0.9000
+# A) 4/7/20 (IB) - Potential simulation scheme for sparrpowR package 0.0.0.9000
 # B) 4/7/20 (IB) - Simulates a Neyman-Scott Cluster Process for cases and a separate Neyman-Scott Cluster Process for controls
 # C) 4/7/20 (IB) - rNeymanScott helpfile: https://www.rdocumentation.org/packages/spatstat/versions/1.62-2/topics/rNeymanScott
 # D) 4/7/20 (IB) - Allows functionality for user-specified prevalence 
 # E) 4/7/20 (IB) - Allows functionality for user-specified, consistent sample sizes 
 # F) 4/8/20 (IB) - Added functionality for iterative simulations
+# F) 4/8/20 (IB) - Moved custom function to companion file "/R_functions/rand_cascon_neymscot.R"
 # ------------------------------------------ #
 
 ############
@@ -27,67 +28,7 @@ library(spatstat)
 # CUSTOM FUNCTION #
 ###################
 
-random.casecon <- function(prevalence, n_total, n_case, n_control, k_case, k_control, e_case, e_control, r_case, r_control, sim_total, ...) {
-  
-  # marked Neyman-Scott process with a consistent sample size
-  random.cluster <- function(n_cluster, ...) {
-    repeat {
-      x <- spatstat::rNeymanScott(...)
-      if (x$n == n_cluster) break
-      }
-    return(x)
-  }
-  
-  # marked daughter ppp for cases
-  rcluster_case <- function(x0, y0, radius, n, types = "case") {
-    X <- spatstat::runifdisc(n, radius, centre=c(x0, y0))
-    spatstat::marks(X) <- types
-    return(X)
-  }
-  
-  # marked daughter ppp for controls
-  rcluster_control <- function(x0, y0, radius, n, types = "control") {
-    X <- spatstat::runifdisc(n, radius, centre=c(x0, y0))
-    spatstat::marks(X) <- types
-    return(X)
-  }
-  
-  pppList <- vector('list', length(sim_total)) # create empty list
-  
-  # Simulate marked ppp for each iteration
-  for(i in 1:sim_total) {
-    
-    # Create random cluster of cases
-    x <- random.cluster(kappa = k_case,
-                        expand = e_case,
-                        rcluster = rcluster_case, 
-                        n = n_case,
-                        radius = r_case,
-                        n_cluster = prevalence*n_total,
-                        ...
-                        )
-    
-    # Create random cluster of controls
-    y <- random.cluster(kappa = k_control,
-                        expand = e_control,
-                        rcluster = rcluster_control, 
-                        n = n_control,
-                        radius = r_control,
-                        n_cluster = (1-prevalence)*n_total,
-                        ...
-                        )
-    
-    # Combine random clusters of cases and controls into one marked ppp
-    z <- spatstat::superimpose(x, y)
-    
-    # Compile ppp into list
-    pppList[[i]] <- z
-    pppList[[i]]
-  }
-  class(pppList) <- c("ppplist", "solist",  "anylist", "listof", "list")
-  
-  return(pppList)
-}
+source(file = paste(getwd(), "/code/R_functions/rand_cascon_neymscot.R", sep = ""))
 
 ######################
 # EXAMPLE SIMULATION #
@@ -124,7 +65,7 @@ random.casecon <- function(prevalence, n_total, n_case, n_control, k_case, k_con
 set.seed(1234)
 
 # Simulate relative clustering
-rand_pts <- random.casecon(prevalence = 0.2, 
+rand_pts <- rand_cascon_neymscot(prevalence = 0.2, 
                            n_total = 1000,
                            n_case = 50,
                            n_control = 200,
