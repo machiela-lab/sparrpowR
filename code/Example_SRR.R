ffe2d0a2e4c43c80013e6eb1055adca61021f140
Ian Buller, PhD, MA <ian.buller@nih.gov>
2020-04-12 23:20:52 -0400

    Created sparrpowR() function
    Combined rand_cascon() and rand_srr() functions into one
    
    Added an example of its implementation to the Example_SRR.R file

--- code/Example_SRR.R
+++ code/Example_SRR.R
@@ -5,11 +5,12 @@
 # Created on: April 8, 2020
 #
 # Recently modified by: @idblr
-# Recently modified on: April 10, 2020
+# Recently modified on: April 12, 2020
 #
 # Notes:
 # A) 4/8/20 (IB) - Uses simulated data from the companion file "Random_Clustering.R"
-# A) 4/10/20 (IB) - Added example calculating summary of iterative simulation 
+# B) 4/10/20 (IB) - Added example calculating summary of iterative simulation 
+# C) 4/12/20 (IB) - Added example for all-in-one sparrpowR() run
 # ------------------------------------------ #
 
 ############
@@ -36,6 +37,7 @@ library(sparr)
 source(file = paste(getwd(), "/code/R_functions/rand_cascon_unifdisc.R", sep = ""))
 source(file = paste(getwd(), "/code/R_functions/lrr_ramp.R", sep = ""))
 source(file = paste(getwd(), "/code/R_functions/rand_srr.R", sep = ""))
+source(file = paste(getwd(), "/code/R_functions/sparrpowR.R", sep = ""))
 
 ######################
 # EXAMPLE SIMULATION #
@@ -269,9 +271,9 @@ legend("bottom", inset = -0.2, ncol = 2, xpd = T,
        pt.cex = 1
 )
 
-###############################
-# ITERATIVE SPATIAL STATISTIC #
-###############################
+#########################################
+# ITERATIVE SPATIAL STATISTIC EXAMPLE 1 #
+#########################################
 
 # Estimate the mean and standard deviation of the log-relative risk surface 
 # Estimate the mean p-value surface and the proportion of iterations that are significant
@@ -304,11 +306,11 @@ end_time <- Sys.time()
 time_srr <- end_time - start_time # n = 10,000 about 3 min
 
 # Create mean log relative risk raster
-rr <- as.data.frame(dplyr::tibble(
-  x = sim_srr$rx,
-  y = sim_srr$ry,
-  rr = sim_srr$rr_mean
-))
+rr <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                  y = sim_srr$ry,
+                                  rr = sim_srr$rr_mean
+                                  )
+                    )
 lrr_narm <- na.omit(rr) # remove NAs
 sp::coordinates(lrr_narm) <- ~ x + y # coordinates
 sp::gridded(lrr_narm) <- TRUE # gridded
@@ -316,11 +318,11 @@ rr_raster <- raster::raster(lrr_narm)
 #plot(rr_raster) # check raster creation
 
 # Create mean p-value raster
-pval <- as.data.frame(dplyr::tibble(
-  x = sim_srr$rx,
-  y = sim_srr$ry,
-  tol = sim_srr$pval_mean
-))
+pval <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                    y = sim_srr$ry,
+                                    tol = sim_srr$pval_mean
+                                    )
+                      )
 lrr_narm <- na.omit(pval) # remove NAs
 sp::coordinates(lrr_narm) <- ~ x + y # coordinates
 sp::gridded(lrr_narm) <- TRUE # gridded
@@ -338,11 +340,11 @@ pval_reclass <- raster::reclassify(pval_raster, c(-Inf, 0.005, 1,
                                    )
 
 # Create standard deviation of log relative risk raster
-rrsd <- as.data.frame(dplyr::tibble(
-  x = sim_srr$rx,
-  y = sim_srr$ry,
-  sd = sim_srr$rr_sd
-))
+rrsd <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                    y = sim_srr$ry,
+                                    sd = sim_srr$rr_sd
+                                    )
+                      )
 lrr_narm <- na.omit(rrsd) # remove NAs
 sp::coordinates(lrr_narm) <- ~ x + y # coordinates
 sp::gridded(lrr_narm) <- TRUE # gridded
@@ -350,11 +352,11 @@ rrsd_raster <- raster::raster(lrr_narm)
 #plot(rrsd_raster) # check raster creation
 
 # Create proportion significant raster
-pvalprop <- as.data.frame(dplyr::tibble(
-  x = sim_srr$rx,
-  y = sim_srr$ry,
-  prop = sim_srr$pval_prop
-))
+pvalprop <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                        y = sim_srr$ry,
+                                        prop = sim_srr$pval_prop
+                                        )
+                          )
 lrr_narm <- na.omit(pvalprop) # remove NAs
 sp::coordinates(lrr_narm) <- ~ x + y # coordinates
 sp::gridded(lrr_narm) <- TRUE # gridded
@@ -397,17 +399,17 @@ fields::image.plot(ramp$lrr,
                                                0,
                                                format(round(max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2, digits = 2), nsmall = 1),
                                                paste(">",format(round(max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T), digits = 2), nsmall = 1),sep = "")
-                   ),
+                                               ),
                    at = c(min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T),
                           min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2,
                           0,
                           max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2,
                           max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)
-                   ),
+                          ),
                    cex.axis = 0.67
                    ),
                    legend.args = list(text = "mean log relative risk", side = 4, line = 2,  cex = 0.67)
-)
+                   )
 
 ### Standard deviation log relative risk
 #### Colors
@@ -498,4 +500,255 @@ plot(pvalprop_reclass,
                       cex.axis = 0.5
      )
 )
+
+#### Categorical output
+plot(pvalprop_reclass,
+     main = paste("Proportion significant above", p_thresh,"threshold", sep = " "),
+     col = c("black", "grey"),
+     axes = F,
+     bty = "o",
+     box = F,
+     axis.args = list(at = c(1,2),
+                      labels = c("insufficient",
+                                 "sufficient"
+                      ), 
+                      cex.axis = 0.5
+     )
+)
+
+#########################################
+# ITERATIVE SPATIAL STATISTIC EXAMPLE 2 #
+#########################################
+
+# Estimate the mean and standard deviation of the log-relative risk surface 
+# Estimate the mean p-value surface and the proportion of iterations that are significant
+# Uses data simulated with same arugments in previous example
+## 1000 total points
+## 3 case clusters
+## 100 case points within each case cluster (300 case points total)
+## 700 control points within window (complete spatial randomness)
+## 0.1 units for radius of each case cluster
+## within a unit square window (0,1),(0,1)
+## case cluster centroids located at (0.25,0.75), (0.5,0.25), & (0.75,0.75)
+## four simulation iterations (only control locations change between iteration)
+
+## Arguments:
+### sim_locs = the pppList of simulated marked planar point patterns
+### upper_tail = user-specified upper tail of a two-tailed significance level
+### lower_tail = user-specified lower tail of a two-tailed significance level
+### resolution = 10 to calculate surfaces in a 10 x 10 grid
+### edge = "diggle" to employ the Diggle method that reweights each observation-specific kernel
+### adapt = F to estimate using fixed smoothing (future direction: explore adaptive smoothing)
+### h0 = NULL for internal estimation of a common oversmoothing bandwidth computed via the sparr::OS() function in the sparr package (can be user specified if want to force same bandwidth across iterations)
+### verbose = F to clean-up presentation 
+### There are other arguments for tuning (mostly for adaptive smoothing), see sparr::risk() helpfile
+
+source(file = paste(getwd(), "/code/R_functions/sparrpowR.R", sep = ""))
+## NOTE: Force the sparr::risk() arguement tolerate = TRUE to always calculate asymptotic p-vlaue surfaces
+start_time <- Sys.time()
+sim_srr <- sparrpowR(x_case = c(0.25, 0.5, 0.75),
+                     y_case = c(0.75, 0.25, 0.75),
+                     n_case = c(100, 100, 100),
+                     n_control = 700,
+                     r_case = c(0.1, 0.1, 0.1),
+                     sim_total = 100,
+                     samp_case = "uniform",
+                     samp_control = "CSR",
+                     win = spatstat::unit.square(), # the default
+                     upper_tail = 0.995, # default = 0.975
+                     lower_tail = 0.005, # default = 0.025
+                     resolution = 10, # try the default 128 for a smoother surface
+                     edge = "diggle",
+                     adapt = F,
+                     h0 = NULL,
+                     verbose = F
+                     )
+end_time <- Sys.time()
+time_srr <- end_time - start_time # n = 10,000 about 12 min
+
+# Create mean log relative risk raster
+rr <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                  y = sim_srr$ry,
+                                  rr = sim_srr$rr_mean
+                                  )
+                    )
+lrr_narm <- na.omit(rr) # remove NAs
+sp::coordinates(lrr_narm) <- ~ x + y # coordinates
+sp::gridded(lrr_narm) <- TRUE # gridded
+rr_raster <- raster::raster(lrr_narm)
+#plot(rr_raster) # check raster creation
+
+# Create mean p-value raster
+pval <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                    y = sim_srr$ry,
+                                    tol = sim_srr$pval_mean
+                                    )
+                      )
+lrr_narm <- na.omit(pval) # remove NAs
+sp::coordinates(lrr_narm) <- ~ x + y # coordinates
+sp::gridded(lrr_narm) <- TRUE # gridded
+pval_raster <- raster::raster(lrr_narm)
+#plot(pval_raster) # check raster creation
+## Reclassify raster of asymptotic p-value surface
+#### Here: two alpha levels, both two-tailed
+#### 0.1 and 0.05
+pval_reclass <- raster::reclassify(pval_raster, c(-Inf, 0.005, 1,
+                                                  0.005, 0.025, 2,
+                                                  0.025, 0.975, 3,
+                                                  0.975, 0.995, 4,
+                                                  0.995, Inf, 5
+                                                  )
+                                   )
+
+# Create standard deviation of log relative risk raster
+rrsd <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                    y = sim_srr$ry,
+                                    sd = sim_srr$rr_sd
+                                    )
+                      )
+lrr_narm <- na.omit(rrsd) # remove NAs
+sp::coordinates(lrr_narm) <- ~ x + y # coordinates
+sp::gridded(lrr_narm) <- TRUE # gridded
+rrsd_raster <- raster::raster(lrr_narm)
+#plot(rrsd_raster) # check raster creation
+
+# Create proportion significant raster
+pvalprop <- as.data.frame(dplyr::tibble(x = sim_srr$rx,
+                                        y = sim_srr$ry,
+                                        prop = sim_srr$pval_prop
+                                        )
+                          )
+lrr_narm <- na.omit(pvalprop) # remove NAs
+sp::coordinates(lrr_narm) <- ~ x + y # coordinates
+sp::gridded(lrr_narm) <- TRUE # gridded
+pvalprop_raster <- raster::raster(lrr_narm)
+#plot(pvalprop_raster) # check raster creation
+## Reclassify raster of proportion significant
+#### Here: power = 80
+p_thresh <- 0.9
+pvalprop_reclass <- raster::reclassify(pvalprop_raster, c(-Inf, p_thresh, 1,
+                                                          p_thresh, Inf, 2
+                                                          )
+                                       )
+
+## Data Visualization
+
+### Example input
+plot(sim_srr$sim, pch = 1, cex = c(0.5,0.1), cols = c("red", "blue"),
+     main = "First iteration of simulated data"
+     )
+
+### Mean log relative risk
+#### Colors for raster
+plot_cols5 <- c("indianred4", "indianred1", "grey80","cornflowerblue","blue3")
+plot_cols3 <- plot_cols5[-c(2,4)] # just the two extreme colors and the color for insignificance
+
+ramp <- lrr_ramp(x = rr_raster, 
+                 cols = plot_cols3,
+                 midpoint = 0, # for a log relative risk midpoint is 0
+                 thresh_up = 5, # likely not necessary because max < thresh_up
+                 thresh_low = -5
+                 )
+#### Plot
+fields::image.plot(ramp$lrr, 
+                   main = "Mean log relative risk",
+                   col = ramp$cols, 
+                   breaks = ramp$breaks,
+                   axes = F,
+                   cex.lab = 1,
+                   xlab = "",
+                   ylab = "",
+                   cex = 1,
+                   smallplot = c(0.86, 0.88, 0.2, 0.8),
+                   axis.args = list(labels = c(paste("<", format(round(min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm = T), digits = 2), nsmall = 1) ,sep = ""),
+                                               format(round(min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2, digits = 2), nsmall = 1),
+                                               0,
+                                               format(round(max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2, digits = 2), nsmall = 1),
+                                               paste(">",format(round(max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T), digits = 2), nsmall = 1),sep = "")
+                                               ),
+                   at = c(min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T),
+                          min(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2,
+                          0,
+                          max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)/2,
+                          max(ramp$lrr[!is.infinite(ramp$lrr)], na.rm=T)
+                          ),
+                   cex.axis = 0.67
+                   ),
+                   legend.args = list(text = "mean log relative risk", side = 4, line = 2,  cex = 0.67)
+                   )
+
+### Standard deviation log relative risk
+#### Colors
+rampcols <- terrain.colors(length(raster::values(rrsd_raster)))
+rampbreaks <- seq(rrsd_raster@data@min, rrsd_raster@data@max, 
+                  length.out = length(raster::values(rrsd_raster))+1
+                  )
+#### Plot
+fields::image.plot(rrsd_raster, 
+                   main = "Standard deviation log relative risk",
+                   col = rampcols, 
+                   breaks = rampbreaks,
+                   axes = F,
+                   cex.lab = 1,
+                   xlab = "",
+                   ylab = "",
+                   cex = 1,
+                   smallplot = c(0.86, 0.88, 0.2, 0.8),
+                   axis.args = list(labels = format(round(seq(rrsd_raster@data@min,
+                                                              rrsd_raster@data@max,
+                                                              (1/6)*rrsd_raster@data@max
+                                                              ), digits = 2), nsmall = 1),
+                   at =  seq(rrsd_raster@data@min,
+                             rrsd_raster@data@max,
+                             (1/6)*rrsd_raster@data@max
+                             ),
+                   cex.axis = 0.67
+                   ),
+                   legend.args = list(text = "standard deviation log relative risk",
+                                      side = 4, line = 2,  cex = 0.67
+                                      )
+                   )
+
+### Mean p-value
+### Categorical output
+plot(pval_reclass,
+     main = "Significant mean p-values\n alpha = 0.1 and alpha = 0.05",
+     col = plot_cols5,
+     axes = F,
+     bty = "o",
+     box = F,
+     axis.args = list(at = seq(1,5,1),
+                      labels = c("<0.005", "<0.025", "insignficant",
+                                 ">0.975",  ">0.995"
+                                 ), 
+                      cex.axis = 0.5
+                      )
+     )
+
+### Proportion of p-value significant
+#### Colors for raster
+rampcols <- rev(gray.colors(length(raster::values(pvalprop_raster)), 
+                            start = 0.95, end = 0, 
+                            gamma = 1, alpha = NULL
+                            )
+                )
+rampbreaks <- seq(pvalprop_raster@data@min, pvalprop_raster@data@max, 
+                  length.out = length(raster::values(pvalprop_raster))+1
+                  )
+#### Continuous Output
+fields::image.plot(pvalprop_raster, 
+                   main = "Proportion significant",
+                   col = rampcols, 
+                   breaks = rampbreaks,
+                   axes = F,
+                   cex.lab = 1,
+                   xlab = "",
+                   ylab = "",
+                   cex = 1,
+                   smallplot = c(0.86, 0.88, 0.2, 0.8),
+                   axis.args = list(cex.axis = 0.67),
+                   legend.args = list(text = "proportion significant",
+                                      side = 4, line = 2,  cex = 0.67
+                                      )
+                   )
 # -------------------- END OF CODE -------------------- #
---
+++ code/R_functions/sparrpowR.R
@@ -0,0 +1,297 @@
+# ------------------------------------------ #
+# Function to Estimate the Power of a Spatial Relative Risk using Simulated Data
+#
+# Created by: Ian Buller, Ph.D., M.A. (GitHub: @idblr)
+# Created on: April 12, 2020
+#
+# Recently modified by:
+# Recently modified on:
+#
+# Notes:
+# A) 4/12/20 (IB) - Combines rand_cascon() and rand_srr() functions
+# ------------------------------------------ #
+
+sparrpowR <- function(x_case, y_case,
+                      n_case = NULL, n_control, n_cluster, n_knot,
+                      r_case, r_control,
+                      l_control, l_case = NULL,
+                      e_control,
+                      sim_total,
+                      samp_case = c("uniform", "Poisson"),
+                      samp_control = c("CSR", "systematic", "stratified",
+                                       "IPP", "clustered"),
+                      same_n = FALSE, 
+                      upper_tail = 0.975,
+                      lower_tail = 0.025,
+                      parallel = FALSE, 
+                      win = unit.square(), ...) {
+  
+  # Packages
+  require(spatstat)
+  require(sparr)
+  require(foreach)
+  require(svMisc)
+  
+  # Custom Internal Functions
+  ## Set function used in foreach
+  if (parallel == TRUE){
+    `%fun%` <- `%dopar%`
+  } else {
+    `%fun%` <- `%do%`
+  }
+  
+  ## Combine function used in foreach
+  comb <- function(x, ...) {
+    lapply(seq_along(x),
+           function(i) c(x[[i]], lapply(list(...), function(y) y[[i]])))
+  }
+  
+  ## Calculate proportion of runs as significant
+  proportionSignificant <- function(x) {
+    x / length(pppList)
+  }
+  
+  # Inputs
+  if (length(x_case) != length(y_case)) {
+    stop("There is at least one missing coordinate")
+  }
+  
+  if (length(x_case) != length(r_case) | length(y_case) != length(r_case)) {
+    stop("There is at least one radius (r_case) missing")
+  }
+  
+  if (samp_case == "Poisson" & length(l_case) == 1) {
+    l_list <- vector('list', length(x_case))
+    for (l in 1:length(x_case)) {
+      l_list[[l]] <- l_case
+    }
+  }
+  
+  # marked uniform disc ppp with user-specified radius for cases
+  rcluster_case <- function(x0, y0, radius, n = NULL, types = "case", lambda = NULL, win = NULL, ...) {
+    
+    if (samp_case == "uniform"){
+      
+      if (length(x_case) != length(n_case)) {
+        stop("There is at least one case cluster sample size (n_case) missing")
+      }
+      
+      repeat {  
+        x <- spatstat::runifdisc(n, radius, centre = c(x0, y0), ...)
+        if (x$n == n) break
+      }
+    }  
+    
+    if (samp_case == "Poisson"){
+      
+      win_case <- spatstat::disc(radius, centre = c(0.5, 0.5), ...)
+      # repeat { 
+      x <- spatstat::rpoispp(lambda, win = win_case, ...)
+      x <- spatstat::shift(x, c(x0 - 0.5, y0 - 0.5))
+      # if (x$n == n) break
+      # }
+    }
+    
+    spatstat::marks(x) <- types
+    return(x)
+  }
+  
+  # marked uniform ppp for controls
+  rcluster_control <- function(n, types = "control", ...) {
+    if (samp_control == "CSR") {
+      repeat {  
+        x <- spatstat::rpoispp(lambda = n, ...)
+        if (x$n == n) break
+      }
+    }
+    
+    if (samp_control == "systematic") {
+      # repeat { 
+      x <- spatstat::rsyst(nx = sqrt(n), ...)
+      # if (x$n == n) break
+      # }
+    }
+    
+    if (samp_control == "stratified") {
+      # repeat {
+      x <- spatstat::rstrat(nx = round(sqrt(n_knot)), k = round(n/n_knot), ...)
+      # if (x$n == n) break
+      # }
+    }
+    
+    if (samp_control == "IPP") {
+      if (class(l_control) != "function") {
+        stop("The argument 'lambda' should be an intensity function")
+      }
+      # repeat {  
+      x <- spatstat::rpoispp(lambda = l_control, ...)
+      # if (x$n == n) break
+      # }
+    }
+    
+    if (samp_control == "clustered") {
+      
+      rcluster_control <- function(x0, y0, radius, n) {
+        X <- spatstat::runifdisc(n, radius, centre=c(x0, y0))
+        return(X)
+      }
+      
+      if(same_n == T){
+        repeat {
+          x <- spatstat::rNeymanScott(kappa = l_control,
+                                      expand = e_control,
+                                      rcluster = rcluster_control, 
+                                      n = n_cluster,
+                                      radius = r_control
+          )
+          if (x$n == n) break
+        }
+      } else {
+        x <- spatstat::rNeymanScott(kappa = l_control,
+                                    expand = e_control,
+                                    rcluster = rcluster_control, 
+                                    n = n_cluster,
+                                    radius = r_control
+        )
+      }
+    }
+    spatstat::marks(x) <- types
+    return(x)
+  }
+  
+  # Create empty lists
+  pppCase <- vector('list', length(x_case))
+  pppList <- vector('list', length(sim_total))
+  
+  # Create a consistent random cluster of cases (uniform around user-specified centroid)
+  for (i in 1:length(x_case)){
+    if(samp_case == "uniform"){
+      x1 <- rcluster_case(x0 = x_case[i], y0 = y_case[i],
+                          radius = r_case[i], n = n_case[i],
+                          lambda = NULL, ...)
+    }
+    
+    if(samp_case == "Poisson"){
+      
+      if (length(l_case) == 1) {
+        x1 <- rcluster_case(x0 = x_case[i], y0 = y_case[i],
+                            radius = r_case[i], n = NULL,
+                            lambda = l_list[[i]], ...)
+      } else {
+        x1 <- rcluster_case(x0 = x_case[i], y0 = y_case[i],
+                            radius = r_case[i], n = NULL,
+                            lambda = l_case[[i]], ...)
+      }
+    }
+    
+    pppCase[[i]] <- x1
+  }
+  class(pppCase) <- c("ppplist", "solist",  "anylist", "listof", "list")
+  x <- spatstat::superimpose(pppCase)
+  
+  # Progress bar
+  message("Step One: Generating Simulated Data")
+  pb <- txtProgressBar(min = 0, max = sim_total, style = 3)
+  
+  # Simulate marked ppp for each iteration
+  for (j in 1:sim_total) {
+    
+    # Progress bar
+    setTxtProgressBar(pb, j)
+    
+    # Create random cluster of controls
+    y <- rcluster_control(n = n_control, ...)
+    
+    # Combine random clusters of cases and controls into one marked ppp
+    z <- spatstat::superimpose(x, y)
+    
+    # Compile ppp into list
+    pppList[[j]] <- z
+    pppList[[j]]
+  }
+  class(pppList) <- c("ppplist", "solist",  "anylist", "listof", "list")
+  
+  # Progress bar
+  message("\nStep Two: Estimating Spatial Relative Risk")
+  
+  # Iteratively calculate the log relative risk and asymptotic p-value surfaces
+  out_par <- foreach::foreach(k = 1:length(pppList), 
+                              .combine = comb, 
+                              .multicombine = TRUE, 
+                              .packages = "sparr",
+                              .init = list(list(), list(), list(), list())
+  ) %fun% {
+    
+    # Progress bar
+    setTxtProgressBar(pb, k)
+    
+    # Calculate observed kernel density ratio
+    obs_lrr <- sparr::risk(pppList[[k]], tolerate = T, ...)
+    
+    # Output processing for visualization and summary across iterations
+    ## Convert output matrix to two output vectors
+    ### Coordinates for each knot
+    rx <- rep(obs_lrr$rr$xcol, length(obs_lrr$rr$yrow))
+    for(i in 1:length(obs_lrr$rr$yrow)){
+      if (i == 1){ ry <- rep(obs_lrr$rr$yrow[i], length(obs_lrr$rr$xcol))}
+      if (i != 1){ ry <- c(ry, rep(obs_lrr$rr$yrow[i], length(obs_lrr$rr$xcol)))}
+    }
+    ### Estimated value (log relative risk and p-value) for each knot
+    if(k == 1) {
+      sim_risk = as.vector(t(obs_lrr$rr$v))
+      sim_pval = as.vector(t(obs_lrr$P$v))
+    } else {
+      rx <- NULL
+      ry <- NULL
+      sim_risk = as.vector(t(obs_lrr$rr$v))
+      sim_pval = as.vector(t(obs_lrr$P$v))
+    }
+    
+    # Output for each n-fold
+    par_results <- list("sim_risk" = sim_risk,
+                        "sim_pval" = sim_pval,
+                        "rx" = rx,
+                        "ry" = ry
+    )
+    
+    return(par_results)
+  }
+  
+  # Summarize iterative results
+  sim_rr <- out_par[[1]]
+  sim_pval <- out_par[[2]]
+  rx <- out_par[[3]][[1]]
+  ry <- out_par[[4]][[1]]
+  
+  ## Set -Inf and Inf to NA values in order to capture in output
+  sim_rr_dat <- do.call(cbind, sim_rr)
+  sim_rr_dat <- ifelse(sim_rr_dat == -Inf, NA, sim_rr_dat)
+  sim_rr_dat <- ifelse(sim_rr_dat == Inf, NA, sim_rr_dat)
+  
+  ## Calculate Mean and Standard Deviation
+  rr_mean <- rowMeans(sim_rr_dat, na.rm = TRUE) # mean log relative risk
+  pval_mean <-  rowMeans(do.call(cbind, sim_pval), na.rm = TRUE) # mean p-value
+  rr_sd <- apply(sim_rr_dat, 1, sd, na.rm = TRUE) # standard deviation log relative risk
+  
+  ## Calculate proportion of tests were significant
+  ### Significance level is user-specified
+  pval_sig <- rapply(sim_pval, function(x) ifelse(x < lower_tail | x > upper_tail , TRUE, FALSE), how = "replace")
+  pval_count <- rowSums(do.call(cbind,pval_sig), na.rm = TRUE)
+  pval_prop_wNA <- sapply(pval_count, FUN = proportionSignificant)
+  
+  ## Force NA values for graphing, match position of NAs of mean p-value
+  pval_prop_wNA <- cbind(pval_mean,pval_prop_wNA)
+  pval_prop_wNA[,2][is.na(pval_prop_wNA[,1])] <- NA
+  pval_prop <- pval_prop_wNA[,2]
+  
+  # Output
+  out_sim <- list("sim" = pppList[[1]],
+                  "rr_mean" = rr_mean,
+                  "pval_mean" = pval_mean,
+                  "rr_sd" = rr_sd,
+                  "pval_prop" = pval_prop,
+                  "rx" = rx,
+                  "ry" = ry
+                  )
+}
+# -------------------- END OF CODE -------------------- #
